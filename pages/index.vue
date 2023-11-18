effd3843dfc2c837890046c7643e7fb6f043f190
Hassen Taidirt <hi@hassen.io>
2023-11-18 21:46:36 +0100

    Basic routing examples

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil tempora amet aspernatur nam similique voluptatum cumque, provident optio aliquam voluptatibus qui, earum id nobis laboriosam commodi error magnam totam iste.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/[id]/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is the page for product {{ id }}</h1>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
