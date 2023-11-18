454984989bbf4bb32e7b580355ef8021b90fe7fd
Hassen Taidirt <hi@hassen.io>
2023-11-19 00:47:21 +0100

    Handle 404 errors

--- pages/products/[id]/index.vue
+++ pages/products/[id]/index.vue
@@ -1,13 +1,21 @@
+<script setup>
+  const { id } = useRoute().params;
+
+  const { data: product } = await useFetch(`http://fakestoreapi.com/products/${id}`);
+
+  if (!product.value) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Product not found',
+      fatal: true,
+    });
+  }
+</script>
+
 <template>
   <div>
     <ProductDetails :product="product" />
   </div>
 </template>
-
-<script setup>
-  const { id } = useRoute().params;
-
-  const { data: product } = await useFetch(`http://fakestoreapi.com/products/${id}`);
-</script>
 
 <style scoped></style>
