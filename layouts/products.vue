82da7d4e0c69967343ee5970933eeaa1e14b7f94
Hassen Taidirt <hi@hassen.io>
2023-11-18 22:13:23 +0100

    Use layouts

---
+++ layouts/products.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">All products</NuxtLink></li>
+          <li><NuxtLink to="/products/123">Product 123</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: #0090ff;
+  }
+</style>
