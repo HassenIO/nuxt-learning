b2d1bea2d9952c59f23d776936aa8bda4ad47bc8
Hassen Taidirt <hi@hassen.io>
2023-11-18 23:24:59 +0100

    Get products from a 3rd party API

--- pages/products/[id]/index.vue
+++ pages/products/[id]/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <h1>This is the page for product {{ id }}</h1>
+    <h1>{{ product.title }}</h1>
+    <p>For just ${{ product.price }}</p>
+    <hr class="my-4"/>
+    <div class="flex mt-4">
+      <p>{{ product.description }}</p>
+      <img :src="product.image" :alt="product.title" class="w-32"/>
+    </div>
   </div>
 </template>
 
 <script setup>
   const { id } = useRoute().params;
+
+  const { data: product } = await useFetch(`http://fakestoreapi.com/products/${id}`);
 </script>
 
 <style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <h1>This is the products page</h1>
+    <ul>
+      <li v-for="product in products" key="product.id">
+        <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
+  const { data: products } = await useFetch('http://fakestoreapi.com/products');
 </script>
 
 <style scoped></style>
