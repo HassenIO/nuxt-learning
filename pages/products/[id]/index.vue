e21400edf15b6f020ed1f086ea6dffb390daa32e
Hassen Taidirt <hi@hassen.io>
2023-11-19 00:14:37 +0100

    Create ProductDetails component

--- pages/products/[id]/index.vue
+++ pages/products/[id]/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <h1>{{ product.title }}</h1>
-    <p>For just ${{ product.price }}</p>
-    <hr class="my-4"/>
-    <div class="flex mt-4">
-      <p>{{ product.description }}</p>
-      <img :src="product.image" :alt="product.title" class="w-32"/>
-    </div>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
