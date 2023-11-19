926f2665ea947a67c362fbbe665fce3a93c43fbf
Hassen Taidirt <hi@hassen.io>
2023-11-19 02:00:05 +0100

    Create a server API

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
+<script setup>
+  const { data } = await useFetch('/api/hello?name=Hassen', {
+    method: 'POST',
+    body: {
+      age: 39,
+    }
+  });
+</script>
+
 <template>
   <div>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil tempora amet aspernatur nam similique voluptatum cumque, provident optio aliquam voluptatibus qui, earum id nobis laboriosam commodi error magnam totam iste.</p>
+    <div>{{ data }}</div>
   </div>
 </template>
 
-<script setup>
-</script>
-
 <style scoped>
-
 </style>
